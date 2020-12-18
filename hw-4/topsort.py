3f6def886b89a6a9c3ba462d9e2cd5e284f34eec
Mehmet Yaylacı <mehmet.yaylaci@ug.bilkent.edu.tr>
2020-12-18 19:03:26 +0300

    final

--- hw-4/topsort.py
+++ hw-4/topsort.py
@@ -3,10 +3,9 @@
 
 Citation for the Code (The Graph Class)
 
-Title: Iterative Topological Sort
-Author: Shihab Shahriar Khan    
-Date: Nov. 11, 2017
-Availability: https://stackoverflow.com/questions/47192626/deceptively-simple-implementation-of-topological-sorting-in-python
+Title: Toposort
+Author: Peter Teichman   
+Availability: https://gist.github.com/pteichman/618ee334321714590cd9
 
 This code was adapted from the previously cited source. Adaptations
 include the conversion of the method into a class method, hence make it
@@ -17,8 +16,10 @@ from collections import defaultdict
 
 class Graph:
     # Constructor for the Graph class
-    def __init__(self):
-        self.graph = {}
+    def __init__(self, V):
+        self.graph = defaultdict(list)
+        self.V = V
+
     '''
     Adds an edge to the dictionary of the class
     Takes in 2 parameters -2 vertices-
@@ -32,41 +33,47 @@ class Graph:
         print("Added " + u + " to " + v, end = ", ")
 
     '''
-    Adds the last edge to the dictionary of the class
-    Takes in the last vertex as the parameter
-    and adds it to the dictionary
-    '''
-    def add_edge_last(self, u):
-        self.graph[u] = []
-        print("Added last node " + u)
-
-    '''
     The topological sorting algorithm
     Uses the graph dictionary directly
     to topologically sort all the edges
     and returns the list in the correct order
     '''
+    def topologicalSort(self):
+        """Perform a topological sort on a graph
+        This returns an ordering of the nodes in the graph that places all
+        dependencies before the nodes that require them.
+        Args:
+            graph: an adjacency dict {node1: [dep1, dep2], node2: [dep1, dep3]}
+        Returns:
+            A list of ordered nodes
+        """
+        result = []
+        used = set()
 
-    def top_sort(self, start):
-        q = [start]
-        ans = []
-        path =  set()
-        while q:
-            v = q[-1]
-            print("Visiting: " + v)
-            path = path.union({v})  
-            children = [x for x in self.graph[v] if x not in path]    
-            if not children:
-                ans = [v]+ans 
-                q.pop()
-            else: 
-                q.append(children[0])
-        print('\n---- OUTPUT ----\n')
-        return ans
+        def use(v, top):
+            print("Visiting {}".format(v))
+
+            if v in used:
+                return
+
+            for parent in self.graph.get(v, []):
+                if parent is top:
+                    raise ValueError("graph is cyclical through", parent)
+
+                use(parent, v)
+
+            print("Adding {}".format(v))
+            used.add(v)
+            result.append(v)
+
+        for v in self.graph:
+            use(v, v)
+
+        return result[::-1]
 
 
 print("\n\n")
-g = Graph()
+g = Graph(8)
 g.add_edge('fstream', 'iostream')
 g.add_edge('ifstream', 'istream')
 g.add_edge('ofstream', 'ostream')
@@ -75,14 +82,13 @@ g.add_edge('iostream', 'ostream')
 g.add_edge('istream', 'ios')
 g.add_edge('ostream', 'ios')
 g.add_edge('ios', 'everything')
-g.add_edge_last('everything')
 
 print("\nFollowing is a Topological Sort of the given graph (fstream)\n")
-for i in g.top_sort('fstream'):
+for i in g.topologicalSort():
     print(i, end=", ")
 
 print("\n\n")
-g = Graph()
+g = Graph(10)
 g.add_edge('Consultant Manager', 'Consultant')
 g.add_edge('Consultant Manager', 'Manager')
 g.add_edge('Director', 'Manager')
@@ -93,14 +99,13 @@ g.add_edge('Manager', 'Employee')
 g.add_edge('Permanent Employee', 'Employee')
 g.add_edge('Temporary Employee', 'Employee')
 g.add_edge('Employee', 'Everything')
-g.add_edge_last('Everything')
 
 print("\nFollowing is a Topological Sort of the given graph (Manager - Employee)\n")
-for i in g.top_sort('Consultant Manager'):
+for i in g.topologicalSort():
     print(i, end=", ")
 
 print("\n\n")
-g = Graph()
+g = Graph(19)
 g.add_edge('Crazy', 'Professors')
 g.add_edge('Crazy', 'Hackers')
 g.add_edge('Jacque', 'Weigthlifters')
@@ -120,8 +125,7 @@ g.add_edge('Programmers', 'Dwarfs')
 g.add_edge('Endomorphs', 'Dwarfs')
 g.add_edge('Athletes', 'Dwarfs')
 g.add_edge('Dwarfs', 'Everything')
-g.add_edge_last('Everything')
 
 print("\nFollowing is a Topological Sort of the given graph (Crazy)\n")
-for i in g.top_sort('Crazy'):
+for i in g.topologicalSort():
     print(i, end=", ")
